321474b153094944aa0b7dc742068a6180b8175b
Ayscue, Zackery T <zackery.ayscue@cengage.com>
2019-05-04 20:58:54 -0400

    Initial Commit

---
+++ VirusScanner.MVC/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using nClam;
+using VirusScanner.MVC.Models;
+
+namespace VirusScanner.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ClamClient _clam;
+
+        public HomeController(ClamClient clam)
+        {
+            _clam = clam;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet("UploadFiles")]
+        public IActionResult UploadFiles()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            var log = new List<ScanResult>();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var ping = await _clam.PingAsync();
+
+                    if(ping)
+                    {
+                        var result = await _clam.SendAndScanFileAsync(formFile.OpenReadStream());
+
+                        log.Add(new ScanResult()
+                        {
+                            FileName = formFile.FileName,
+                            Result = result.Result.ToString(),
+                            Message = result.InfectedFiles?.FirstOrDefault()?.VirusName,
+                            RawResult = result.RawResult
+                        });
+                    }
+                }
+            }
+
+            var model = new UploadFilesViewModel
+            {
+                Results = log
+            };
+
+            return View("UploadResults", model);
+        }
+    }
+}
---
+++ VirusScanner.MVC/Models/UploadFilesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VirusScanner.MVC.Models
+{
+    public class UploadFilesViewModel
+    {
+        public IList<ScanResult> Results { get; set; }
+    }
+
+    public class ScanResult
+    {
+        public string FileName { get; set; }
+        public string Result { get; set; }
+        public string Message { get; set; }
+        public string RawResult { get; set; }
+    }
+}
